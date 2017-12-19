0771c11c677da0de06993efc8abc650469dbabc6
simon <simon@172.16.2.164>
2017-12-19 19:18:08 -0300

    Version 1.1 RRHH

--- src/Negocio/Personal.java
+++ src/Negocio/Personal.java
@@ -10,5 +10,84 @@ package Negocio;
  * @author simon
  */
 public class Personal {
-    
+    int id, rut;
+    String dv, fechaIngreso,direccion,fechaCumple,nombre,apellido;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getDv() {
+        return dv;
+    }
+
+    public void setDv(String dv) {
+        this.dv = dv;
+    }
+
+    public String getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(String fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFechaCumple() {
+        return fechaCumple;
+    }
+
+    public void setFechaCumple(String fechaCumple) {
+        this.fechaCumple = fechaCumple;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Personal(int id, int rut, String dv, String fechaIngreso, String direccion, String fechaCumple, String nombre, String apellido) {
+        this.id = id;
+        this.rut = rut;
+        this.dv = dv;
+        this.fechaIngreso = fechaIngreso;
+        this.direccion = direccion;
+        this.fechaCumple = fechaCumple;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Personal() {
+    }
 }
